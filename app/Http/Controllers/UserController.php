2bb0872f8e27c037f0874866ff7c1bba23617b20
RomaSub <antonfrolov1212@gmail.com>
2024-07-02 11:50:03 +0300

    fix

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -14,7 +14,7 @@ class UserController extends Controller
      */
     public function index()
     {
-        $users = User::latest()->paginate(15);
+        $users = User::orderBy('updated_at', 'desc')->paginate(15);
 
         return inertia('Home', ['users' => $users]);
     }
